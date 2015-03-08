9a6ccac1315a99b28b763b00e81aaf9f4c6a1201
Johannes Altmanninger <aclopte@gmail.com>
2015-03-09 00:04:10 +0100

    Slovenian translation (thanks to silverhook)

---
+++ translations/cyanide-sl.ts
@@ -0,0 +1,18 @@
+<?xml version='1.0' encoding='utf-8'?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Friend</name>
+    <message>
+        <source>Remove friend</source>
+        <translation>Odstrani prijatelja</translation>
+    </message>
+</context>
+<context>
+    <name>FriendList</name>
+    <message>
+        <source>Add a friend</source>
+        <translation>Dodaj prijatelja</translation>
+    </message>
+</context>
+</TS>
