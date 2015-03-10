5cd243442dd6b08c6841125d928839e7b04b7ab6
Johannes Altmanninger <aclopte@gmail.com>
2015-03-10 18:11:41 +0100

    Slovenian transation

--- translations/cyanide-sl.ts
+++ translations/cyanide-sl.ts
@@ -2,10 +2,40 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>AcceptFriend</name>
+    <message>
+        <source>Accept</source>
+        <translation>Sprejmi</translation>
+    </message>
+    <message>
+        <source>Ignore</source>
+        <translation>Prezri</translation>
+    </message>
+</context>
+<context>
+    <name>AddFriend</name>
+    <message>
+        <source>Tox me maybe?</source>
+        <translation type="unfinished">Toxaj z mano…</translation>
+    </message>
+    <message>
+        <source>Send friend request</source>
+        <translation>Pošlji zahtevo za prijateljstvo</translation>
+    </message>
+</context>
+<context>
     <name>Friend</name>
     <message>
         <source>Remove friend</source>
         <translation>Odstrani prijatelja</translation>
+    </message>
+    <message>
+        <source>About to remove friend</source>
+        <translation type="unfinished">Prijatelj bo odstranjen</translation>
+    </message>
+    <message>
+        <source>Launching web browser...</source>
+        <translation>Odpiram spletni brskalnik</translation>
     </message>
 </context>
 <context>
@@ -13,6 +43,44 @@
     <message>
         <source>Add a friend</source>
         <translation>Dodaj prijatelja</translation>
+    </message>
+    <message>
+        <source>Friends</source>
+        <translation>Prijatelji</translation>
+    </message>
+</context>
+<context>
+    <name>Profile</name>
+    <message>
+        <source>Name</source>
+        <translation>Ime</translation>
+    </message>
+    <message>
+        <source>Status Message</source>
+        <translation>Sporočilo stanja</translation>
+    </message>
+    <message>
+        <source>Copy my Tox ID</source>
+        <translation>Kopiraj moj Tox ID</translation>
+    </message>
+    <message>
+        <source>Online</source>
+        <translation>Povezan</translation>
+    </message>
+    <message>
+        <source>Away</source>
+        <translation>Odsoten</translation>
+    </message>
+    <message>
+        <source>Busy</source>
+        <translation>Zaposlen (ne zaseden)</translation>
+    </message>
+</context>
+<context>
+    <name>cyanide</name>
+    <message>
+        <source> is now known as </source>
+        <translation type="unfinished"> je sedaj znan kot </translation>
     </message>
 </context>
 </TS>
