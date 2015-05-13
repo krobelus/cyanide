549ea7ea022003362824786f1c1bce157601af79
Peter Jespersen <flywheel@illogical.dk>
2015-05-13 16:27:28 +0200

    Translated using Weblate (Danish)
    Currently translated at 100.0% (67 of 67 strings)

--- translations/cyanide-da.ts
+++ translations/cyanide-da.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="da">
 <context>
@@ -22,7 +22,7 @@
         <location filename="../qml/pages/AddFriend.qml" line="39"/>
         <source>Tox me maybe?</source>
         <extracomment>default friend request message - feel free to tranlate freely</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Tox mig ?</translation>
     </message>
     <message>
         <location filename="../qml/pages/AddFriend.qml" line="46"/>
@@ -32,7 +32,7 @@
     <message>
         <location filename="../qml/pages/AddFriend.qml" line="58"/>
         <source>Failed to send friend request</source>
-        <translation>Fejl ved afsending af venneanmodning</translation>
+        <translation>Fejl ved afsendelse af venneanmodning</translation>
     </message>
 </context>
 <context>
@@ -40,7 +40,7 @@
     <message>
         <location filename="../src/cyanide.cpp" line="820"/>
         <source>Error: Invalid Tox ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Ugyldig ToxID</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="848"/>
@@ -50,103 +50,103 @@
     <message>
         <location filename="../src/cyanide.cpp" line="852"/>
         <source>Error: Tox ID is self ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: ToxID er dit eget ID</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="854"/>
         <source>Error: Tox ID is already in friend list</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: ToxID er allerede på venneliste</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="856"/>
         <source>Error: Invalid Tox ID (bad checksum)</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Ugyldigt ToxID (ugyldig checksum)</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="858"/>
         <source>Error: Invalid Tox ID (bad nospam value)</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Ugyldig ToxID (ugyldig nospam værdi)</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="860"/>
         <source>Error: No memory</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Hukommelse opbrugt</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="923"/>
         <location filename="../src/filetransfers.cpp" line="422"/>
         <location filename="../src/filetransfers.cpp" line="530"/>
         <source>Error: Friend not connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Ven ikke tilsluttet</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1001"/>
         <source>Error: File exists</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Filen eksisterer allerede</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1162"/>
         <source>File not found: </source>
-        <translation type="unfinished"></translation>
+        <translation>Filen er ikke fundet: </translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1167"/>
         <source>Avatar too large. Maximum size: 64KiB</source>
-        <translation type="unfinished"></translation>
+        <translation>Avatar er for stor. Maks. størrelse: 64kB</translation>
     </message>
     <message>
         <location filename="../src/cyanide.h" line="221"/>
         <source>Tox User</source>
         <extracomment>default username</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Toxbruger</translation>
     </message>
     <message>
         <location filename="../src/cyanide.h" line="223"/>
         <source>Toxing on Cyanide</source>
         <extracomment>default status</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Toxer med Cyanide</translation>
     </message>
     <message>
         <location filename="../src/cyanide.h" line="225"/>
         <source>Tox me maybe?</source>
         <extracomment>default friend request message - feel free to tranlate freely</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Tox mig?</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="420"/>
         <location filename="../src/filetransfers.cpp" line="528"/>
         <source>Error: Friend not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Ven ikke fundet</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="424"/>
         <source>Error: File transfer not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl:  Filoverførsel ikke fundet</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="430"/>
         <source>Error: Already paused</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Allerede pauset</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="432"/>
         <source>Error: Packet queue is full</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Pakkekø er fuld</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="509"/>
         <source>Error: Failed to open file: </source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Kan ikke åbne filen: </translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="532"/>
         <source>Error: Filename too long</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: Filnavn er for langt</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="534"/>
         <source>Error: Too many ongoing transfers</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejl: For mange samtidige overførsler</translation>
     </message>
 </context>
 <context>
@@ -154,33 +154,33 @@
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="41"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Afbryd</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="56"/>
         <source>Filter by filename extension</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtrér efter fil-endelse</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="66"/>
         <source>Remove my avatar</source>
-        <translation type="unfinished"></translation>
+        <translation>Fjern min avatar</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="73"/>
         <location filename="../qml/pages/FileChooser.qml" line="125"/>
         <source>Failed to set avatar</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke sætte avatar</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="78"/>
         <source>New profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Ny profil</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="118"/>
         <source>Failed to send file</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan ikke sende filen</translation>
     </message>
 </context>
 <context>
@@ -188,37 +188,37 @@
     <message>
         <location filename="../qml/pages/Friend.qml" line="56"/>
         <source>Failed to pause transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan ikke pause overførsel</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="63"/>
         <source>Failed to resume transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan ikke genoptage overførsel</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="84"/>
         <source>Failed to cancel transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan ikke afbryde overførsel</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="221"/>
         <source> is typing...</source>
-        <translation type="unfinished"></translation>
+        <translation> skriver...</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="299"/>
         <source>Opening URL...</source>
-        <translation type="unfinished"></translation>
+        <translation>Åbner URL...</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="311"/>
         <source>Opening file...</source>
-        <translation type="unfinished"></translation>
+        <translation>Åbner fil...</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="390"/>
         <source>Failed to send message</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan ikke sende meddelelse</translation>
     </message>
 </context>
 <context>
@@ -226,32 +226,32 @@
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="59"/>
         <source>Copy Tox ID to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopiér ToxID til udklipsholder</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="67"/>
         <source>Clear chat log</source>
-        <translation type="unfinished"></translation>
+        <translation>Ryd chatlog</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="76"/>
         <source>Unblock friend</source>
-        <translation type="unfinished"></translation>
+        <translation>Fjern blokering af ven</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="76"/>
         <source>Block friend</source>
-        <translation type="unfinished"></translation>
+        <translation>Blokér ven</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="83"/>
         <source>Remove friend</source>
-        <translation type="unfinished"></translation>
+        <translation>Fjern ven</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="85"/>
         <source>Removing friend</source>
-        <translation type="unfinished"></translation>
+        <translation>Fjern ven</translation>
     </message>
 </context>
 <context>
@@ -259,17 +259,17 @@
     <message>
         <location filename="../qml/pages/FriendList.qml" line="25"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Indstillinger</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendList.qml" line="29"/>
         <source>Switch profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Skift profil</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendList.qml" line="40"/>
         <source>Add a friend</source>
-        <translation type="unfinished"></translation>
+        <translation>Tilføj en ven</translation>
     </message>
 </context>
 <context>
@@ -277,50 +277,50 @@
     <message>
         <location filename="../qml/pages/Profile.qml" line="24"/>
         <source>Delete profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Slet profil</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="26"/>
         <source>Deleting profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Sletter profilen</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="48"/>
         <source>Profile Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Profilnavn</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="63"/>
         <source>Name</source>
         <extracomment>username for this profile</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Navn</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="75"/>
         <source>Status Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Statusmeddelelse</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="92"/>
         <source>Online</source>
         <extracomment>user status</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Online</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="93"/>
         <source>Away</source>
-        <translation type="unfinished"></translation>
+        <translation>Væk</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="94"/>
         <source>Busy</source>
-        <translation type="unfinished"></translation>
+        <translation>Optaget</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="125"/>
         <location filename="../qml/pages/Profile.qml" line="128"/>
         <source>Copy my Tox ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopier mit ToxID</translation>
     </message>
 </context>
 <context>
@@ -328,42 +328,42 @@
     <message>
         <location filename="../qml/pages/Settings.qml" line="28"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Indstillinger</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="19"/>
         <source>Keep chat history</source>
-        <translation type="unfinished"></translation>
+        <translation>Bevar chathistorik</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="29"/>
         <source>Notify me when...</source>
-        <translation type="unfinished"></translation>
+        <translation>Påmind mig når...</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="30"/>
         <source>I receive a message</source>
-        <translation type="unfinished"></translation>
+        <translation>Jeg modtog en meddelelse</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="32"/>
         <source>I receive a friend request</source>
-        <translation type="unfinished"></translation>
+        <translation>Jeg modtog en venneanmodning</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="38"/>
         <source>Send typing notifications?</source>
-        <translation type="unfinished"></translation>
+        <translation>Sende skrivestatus påmindelse?</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="40"/>
         <source>Enable UDP</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktivér UDP</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="42"/>
         <source>Wifi only</source>
-        <translation type="unfinished"></translation>
+        <translation>Kun ved WiFi</translation>
     </message>
 </context>
 <context>
@@ -371,12 +371,12 @@
     <message>
         <location filename="../qml/cyanide.qml" line="189"/>
         <source>Incoming file: </source>
-        <translation type="unfinished"></translation>
+        <translation>Indkommen fil: </translation>
     </message>
     <message>
         <location filename="../qml/cyanide.qml" line="220"/>
         <source>Friend request received!</source>
-        <translation type="unfinished"></translation>
+        <translation>Venneanmodning modtaget!</translation>
     </message>
 </context>
 </TS>
