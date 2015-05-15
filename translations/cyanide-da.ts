832178eecaa725ccf2139c5d3876f22827e7d590
Peter Jespersen <flywheel@illogical.dk>
2015-05-15 21:11:20 +0200

    Translated using Weblate (Danish)
    Currently translated at 100.0% (72 of 72 strings)

--- translations/cyanide-da.ts
+++ translations/cyanide-da.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="da">
 <context>
@@ -237,18 +237,18 @@
         <location filename="../qml/pages/FriendAction.qml" line="71"/>
         <source>Load</source>
         <extracomment>chat history</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Hent</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="75"/>
         <source>Delete</source>
         <extracomment>chat history</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Slet</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="82"/>
         <source>Deleting chat history</source>
-        <translation type="unfinished"></translation>
+        <translation>Slet chathistorik</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="94"/>
@@ -294,12 +294,12 @@
     <message>
         <location filename="../qml/pages/LoadChatHistory.qml" line="39"/>
         <source>From</source>
-        <translation type="unfinished"></translation>
+        <translation>Fra</translation>
     </message>
     <message>
         <location filename="../qml/pages/LoadChatHistory.qml" line="56"/>
         <source>To</source>
-        <translation type="unfinished"></translation>
+        <translation>Til</translation>
     </message>
 </context>
 <context>
