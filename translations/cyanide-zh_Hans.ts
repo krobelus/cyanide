11856989d6480f0304254128c6b41e2f720b216f
quininer kel <quininer@live.com>
2015-07-25 12:25:14 +0200

    Translated using Weblate (Simplified Chinese)
    Currently translated at 87.8% (65 of 74 strings)

--- translations/cyanide-zh_Hans.ts
+++ translations/cyanide-zh_Hans.ts
@@ -1,19 +1,19 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="zh_Hans">
 <context>
     <name>AcceptFriend</name>
     <message>
         <location filename="../qml/pages/AcceptFriend.qml" line="42"/>
         <source>Accept</source>
         <extracomment>Friend request</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>接受</translation>
     </message>
     <message>
         <location filename="../qml/pages/AcceptFriend.qml" line="54"/>
         <source>Ignore</source>
         <extracomment>Friend request</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>忽略</translation>
     </message>
 </context>
 <context>
@@ -22,17 +22,17 @@
         <location filename="../qml/pages/AddFriend.qml" line="39"/>
         <source>Tox me maybe?</source>
         <extracomment>default friend request message - feel free to tranlate freely</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>在 Tox 上联系我？</translation>
     </message>
     <message>
         <location filename="../qml/pages/AddFriend.qml" line="43"/>
         <source>Send friend request</source>
-        <translation type="unfinished"></translation>
+        <translation>发送好友请求</translation>
     </message>
     <message>
         <location filename="../qml/pages/AddFriend.qml" line="55"/>
         <source>Failed to send friend request</source>
-        <translation type="unfinished"></translation>
+        <translation>好友请求发送失败</translation>
     </message>
 </context>
 <context>
@@ -40,113 +40,113 @@
     <message>
         <location filename="../src/cyanide.cpp" line="930"/>
         <source>Error: Invalid Tox ID</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 无效的 Tox ID</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="958"/>
         <source>Error: Message is too long</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 消息太长</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="962"/>
         <source>Error: Tox ID is self ID</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 不能添加自己为好友</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="964"/>
         <source>Error: Tox ID is already in friend list</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: Tox ID 已经在好友列表里</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="966"/>
         <source>Error: Invalid Tox ID (bad checksum)</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 无效的 Tox ID (错误的 checksum)</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="968"/>
         <source>Error: Invalid Tox ID (bad nospam value)</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 无效的 Tox ID (错误的 nospam 值)</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="970"/>
         <source>Error: No memory</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">错误: 没有内存</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1033"/>
         <location filename="../src/filetransfers.cpp" line="418"/>
         <location filename="../src/filetransfers.cpp" line="530"/>
         <source>Error: Friend not connected</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 未连接到好友</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1123"/>
         <source>Error: File exists</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 文件已存在</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1308"/>
         <source>File not found: </source>
-        <translation type="unfinished"></translation>
+        <translation>文件未找到: </translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1313"/>
         <source>Avatar too large. Maximum size: 64KiB</source>
-        <translation type="unfinished"></translation>
+        <translation>头像太大，最大尺寸: 64KiB</translation>
     </message>
     <message>
         <location filename="../src/cyanide.h" line="225"/>
         <source>Tox User</source>
         <extracomment>default username</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Tox 用户</translation>
     </message>
     <message>
         <location filename="../src/cyanide.h" line="227"/>
         <source>Toxing on Cyanide</source>
         <extracomment>default status</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>正在 Cyanide 上使用 Tox</translation>
     </message>
     <message>
         <location filename="../src/cyanide.h" line="229"/>
         <source>Tox me maybe?</source>
         <extracomment>default friend request message - feel free to tranlate freely</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>在 Tox 上联系我？</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="416"/>
         <location filename="../src/filetransfers.cpp" line="528"/>
         <source>Error: Friend not found</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 好友未找到</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="420"/>
         <source>Error: File transfer not found</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">错误: 文件传输未找到</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="426"/>
         <source>Error: Already paused</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">错误: 已暂停</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="428"/>
         <source>Error: Packet queue is full</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 数据包队列已满</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="507"/>
         <source>Error: Failed to open file: </source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 打开文件失败: </translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="532"/>
         <source>Error: Filename too long</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 文件名太长</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="534"/>
         <source>Error: Too many ongoing transfers</source>
-        <translation type="unfinished"></translation>
+        <translation>错误: 正在进行太多文件传输</translation>
     </message>
 </context>
 <context>
@@ -154,7 +154,7 @@
     <message>
         <location filename="../qml/pages/EnterPassword.qml" line="24"/>
         <source>Enter password</source>
-        <translation type="unfinished"></translation>
+        <translation>输入密码</translation>
     </message>
 </context>
 <context>
@@ -162,38 +162,38 @@
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="41"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>取消</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="56"/>
         <source>Filter by filename extension</source>
-        <translation type="unfinished"></translation>
+        <translation>根据文件扩展名过滤</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="66"/>
         <source>Remove my avatar</source>
-        <translation type="unfinished"></translation>
+        <translation>移除我的头像</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="73"/>
         <location filename="../qml/pages/FileChooser.qml" line="127"/>
         <source>Failed to set avatar</source>
-        <translation type="unfinished"></translation>
+        <translation>设置头像失败</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="78"/>
         <source>New profile</source>
-        <translation type="unfinished"></translation>
+        <translation>新账户</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="120"/>
         <source>Failed to send file</source>
-        <translation type="unfinished"></translation>
+        <translation>发送文件失败</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="141"/>
         <source>Decryption failed</source>
-        <translation type="unfinished"></translation>
+        <translation>解密失败</translation>
     </message>
 </context>
 <context>
@@ -201,37 +201,37 @@
     <message>
         <location filename="../qml/pages/Friend.qml" line="76"/>
         <source>Failed to pause transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>暂停传输失败</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="83"/>
         <source>Failed to resume transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>恢复传输失败</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="104"/>
         <source>Failed to cancel transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>取消传输失败</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="242"/>
         <source> is typing...</source>
-        <translation type="unfinished"></translation>
+        <translation> 正在输入...</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="343"/>
         <source>Opening URL...</source>
-        <translation type="unfinished"></translation>
+        <translation>打开 URL...</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="354"/>
         <source>Opening file...</source>
-        <translation type="unfinished"></translation>
+        <translation>打开文件...</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="416"/>
         <source>Failed to send message</source>
-        <translation type="unfinished"></translation>
+        <translation>发送消息失败</translation>
     </message>
 </context>
 <context>
@@ -239,49 +239,49 @@
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="73"/>
         <source>Copy Tox ID to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>已复制 Tox ID 到剪切板</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="83"/>
         <source>Chat history</source>
-        <translation type="unfinished"></translation>
+        <translation>聊天历史</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="88"/>
         <source>Load</source>
         <extracomment>chat history</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>加载</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="92"/>
         <source>Delete</source>
         <extracomment>chat history</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>删除</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="99"/>
         <source>Deleting chat history</source>
-        <translation type="unfinished"></translation>
+        <translation>正在删除聊天历史</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="110"/>
         <source>Unblock friend</source>
-        <translation type="unfinished"></translation>
+        <translation>解除屏蔽好友</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="110"/>
         <source>Block friend</source>
-        <translation type="unfinished"></translation>
+        <translation>屏蔽好友</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="116"/>
         <source>Remove friend</source>
-        <translation type="unfinished"></translation>
+        <translation>移除好友</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="118"/>
         <source>Removing friend</source>
-        <translation type="unfinished"></translation>
+        <translation>正在移除好友</translation>
     </message>
 </context>
 <context>
@@ -289,17 +289,17 @@
     <message>
         <location filename="../qml/pages/FriendList.qml" line="25"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>设置</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendList.qml" line="29"/>
         <source>Switch profile</source>
-        <translation type="unfinished"></translation>
+        <translation>选择账户</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendList.qml" line="40"/>
         <source>Add a friend</source>
-        <translation type="unfinished"></translation>
+        <translation>添加好友</translation>
     </message>
 </context>
 <context>
@@ -307,12 +307,12 @@
     <message>
         <location filename="../qml/pages/LoadChatHistory.qml" line="39"/>
         <source>From</source>
-        <translation type="unfinished"></translation>
+        <translation>来自</translation>
     </message>
     <message>
         <location filename="../qml/pages/LoadChatHistory.qml" line="56"/>
         <source>To</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">对</translation>
     </message>
 </context>
 <context>
@@ -320,50 +320,50 @@
     <message>
         <location filename="../qml/pages/Profile.qml" line="28"/>
         <source>Delete profile</source>
-        <translation type="unfinished"></translation>
+        <translation>删除账户</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="30"/>
         <source>Deleting profile</source>
-        <translation type="unfinished"></translation>
+        <translation>正在删除账户</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="53"/>
         <source>Profile Name</source>
-        <translation type="unfinished"></translation>
+        <translation>账户名</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="68"/>
         <source>Name</source>
         <extracomment>username for this profile</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>名字</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="80"/>
         <source>Status Message</source>
-        <translation type="unfinished"></translation>
+        <translation>状态消息</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="97"/>
         <source>Online</source>
         <extracomment>user status</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>在线</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="98"/>
         <source>Away</source>
-        <translation type="unfinished"></translation>
+        <translation>离开</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="99"/>
         <source>Busy</source>
-        <translation type="unfinished"></translation>
+        <translation>忙碌</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="145"/>
         <location filename="../qml/pages/Profile.qml" line="148"/>
         <source>Copy my Tox ID</source>
-        <translation type="unfinished"></translation>
+        <translation>复制我的 Tox ID</translation>
     </message>
 </context>
 <context>
@@ -371,42 +371,42 @@
     <message>
         <location filename="../qml/pages/Settings.qml" line="30"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>设置</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="18"/>
         <source>Wifi only</source>
-        <translation type="unfinished"></translation>
+        <translation>仅在使用 Wifi 时</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="20"/>
         <source>Send typing notifications?</source>
-        <translation type="unfinished"></translation>
+        <translation>发送"正在输入"通知吗？</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="22"/>
         <source>Keep chat history</source>
-        <translation type="unfinished"></translation>
+        <translation>保存聊天历史</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="24"/>
         <source>Enable UDP</source>
-        <translation type="unfinished"></translation>
+        <translation>开启 UDP</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="26"/>
         <source>Notify me when...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">当... 时提醒我</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="27"/>
         <source>I receive a message</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">我收到一个消息</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="29"/>
         <source>I receive a friend request</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">我收到一个好友请求</translation>
     </message>
 </context>
 <context>
@@ -414,12 +414,12 @@
     <message>
         <location filename="../qml/cyanide.qml" line="218"/>
         <source>Incoming file: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">传入的文件: </translation>
     </message>
     <message>
         <location filename="../qml/cyanide.qml" line="249"/>
         <source>Friend request received!</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">收到好友请求！</translation>
     </message>
 </context>
 </TS>
