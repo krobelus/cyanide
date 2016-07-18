d60597b262506d15ff9c3e6f8de01c591ff93ad7
Kristjan Räts <kristjanrats@gmail.com>
2016-07-18 14:24:38 +0000

    Translated using Weblate (Estonian)
    Currently translated at 100.0% (77 of 77 strings)

--- translations/cyanide-et.ts
+++ translations/cyanide-et.ts
@@ -50,103 +50,103 @@
     <message>
         <location filename="../src/cyanide.cpp" line="973"></location>
         <source>Error: Tox ID is self ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Tox ID on enda ID</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="975"></location>
         <source>Error: Tox ID is already in friend list</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Tox ID on juba sõprade loendis</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="977"></location>
         <source>Error: Invalid Tox ID (bad checksum)</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Kehtetu Tox ID (halb kontrollsumma)</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="979"></location>
         <source>Error: Invalid Tox ID (bad nospam value)</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Kehtetu Tox ID (halb NoSpam väärtus)</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="981"></location>
         <source>Error: No memory</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Mälu otsas</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1044"></location>
         <location filename="../src/filetransfers.cpp" line="420"></location>
         <location filename="../src/filetransfers.cpp" line="532"></location>
         <source>Error: Friend not connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Sõber ei ole ühendatud</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1134"></location>
         <source>Error: File exists</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Fail on olemas</translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1313"></location>
         <source>File not found: </source>
-        <translation type="unfinished"></translation>
+        <translation>Faili ei leitud: </translation>
     </message>
     <message>
         <location filename="../src/cyanide.cpp" line="1318"></location>
         <source>Avatar too large. Maximum size: 64KiB</source>
-        <translation type="unfinished"></translation>
+        <translation>Avatar on liiga suur. Maksimaalne suurus: 64KiB</translation>
     </message>
     <message>
         <location filename="../src/cyanide.h" line="244"></location>
         <source>Tox User</source>
         <extracomment>default username</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Tox kasutaja</translation>
     </message>
     <message>
         <location filename="../src/cyanide.h" line="246"></location>
         <source>Toxing on Cyanide</source>
         <extracomment>default status</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Cyanide Tox</translation>
     </message>
     <message>
         <location filename="../src/cyanide.h" line="248"></location>
         <source>Tox me maybe?</source>
         <extracomment>default friend request message - feel free to tranlate freely</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Võib-olla Toxi mind?</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="418"></location>
         <location filename="../src/filetransfers.cpp" line="530"></location>
         <source>Error: Friend not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Sõpra ei leitud</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="422"></location>
         <source>Error: File transfer not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Faili ülekannet ei leitud</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="428"></location>
         <source>Error: Already paused</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Juba peatatud</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="430"></location>
         <source>Error: Packet queue is full</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Pakettide järjekord on täis</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="509"></location>
         <source>Error: Failed to open file: </source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Faili avamine nurjus: </translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="534"></location>
         <source>Error: Filename too long</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Failinimi on liiga pikk</translation>
     </message>
     <message>
         <location filename="../src/filetransfers.cpp" line="536"></location>
         <source>Error: Too many ongoing transfers</source>
-        <translation type="unfinished"></translation>
+        <translation>Viga: Liiga palju käimasolevaid ülekandeid</translation>
     </message>
 </context>
 <context>
@@ -154,7 +154,7 @@
     <message>
         <location filename="../qml/pages/EnterPassword.qml" line="24"></location>
         <source>Enter password</source>
-        <translation type="unfinished"></translation>
+        <translation>Sisesta salasõna</translation>
     </message>
 </context>
 <context>
@@ -162,38 +162,38 @@
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="41"></location>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Loobu</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="56"></location>
         <source>Filter by filename extension</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtreeri failinime laiendi järgi</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="66"></location>
         <source>Remove my avatar</source>
-        <translation type="unfinished"></translation>
+        <translation>Eemalda mu avatar</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="73"></location>
         <location filename="../qml/pages/FileChooser.qml" line="127"></location>
         <source>Failed to set avatar</source>
-        <translation type="unfinished"></translation>
+        <translation>Avatari seadmine nurjus</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="78"></location>
         <source>New profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Uus profiil</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="120"></location>
         <source>Failed to send file</source>
-        <translation type="unfinished"></translation>
+        <translation>Faili saatmine nurjus</translation>
     </message>
     <message>
         <location filename="../qml/pages/FileChooser.qml" line="141"></location>
         <source>Decryption failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Lahtikrüptimine nurjus</translation>
     </message>
 </context>
 <context>
@@ -201,49 +201,49 @@
     <message>
         <location filename="../qml/pages/Friend.qml" line="77"></location>
         <source>Failed to pause transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>Ülekande peatamine nurjus</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="84"></location>
         <source>Failed to resume transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>Ülekande jätkamine nurjus</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="105"></location>
         <source>Failed to cancel transfer</source>
-        <translation type="unfinished"></translation>
+        <translation>Ülekande tühistamine nurjus</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="162"></location>
         <source>Answer</source>
         <extracomment>answer the call</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Vasta</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="186"></location>
         <source>Hang Up</source>
         <extracomment>hang up</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Lõpeta</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="240"></location>
         <source> is typing...</source>
-        <translation type="unfinished"></translation>
+        <translation> kirjutab...</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="341"></location>
         <source>Opening URL...</source>
-        <translation type="unfinished"></translation>
+        <translation>Avatakse URL...</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="352"></location>
         <source>Opening file...</source>
-        <translation type="unfinished"></translation>
+        <translation>Faili avamine...</translation>
     </message>
     <message>
         <location filename="../qml/pages/Friend.qml" line="414"></location>
         <source>Failed to send message</source>
-        <translation type="unfinished"></translation>
+        <translation>Sõnumi saatmine nurjus</translation>
     </message>
 </context>
 <context>
@@ -251,49 +251,49 @@
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="75"></location>
         <source>Copy Tox ID to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopeeri lõikelauale Tox ID</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="85"></location>
         <source>Chat history</source>
-        <translation type="unfinished"></translation>
+        <translation>Vestluse ajalugu</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="90"></location>
         <source>Load</source>
         <extracomment>chat history</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Laadi</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="94"></location>
         <source>Delete</source>
         <extracomment>chat history</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Kustuta</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="101"></location>
         <source>Deleting chat history</source>
-        <translation type="unfinished"></translation>
+        <translation>Kustuta vestluse ajalugu</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="112"></location>
         <source>Unblock friend</source>
-        <translation type="unfinished"></translation>
+        <translation>Lõpeta sõbra blokeering</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="112"></location>
         <source>Block friend</source>
-        <translation type="unfinished"></translation>
+        <translation>Blokeeri sõber</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="118"></location>
         <source>Remove friend</source>
-        <translation type="unfinished"></translation>
+        <translation>Eemalda sõber</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendAction.qml" line="120"></location>
         <source>Removing friend</source>
-        <translation type="unfinished"></translation>
+        <translation>Sõbra eemaldamine</translation>
     </message>
 </context>
 <context>
@@ -301,17 +301,17 @@
     <message>
         <location filename="../qml/pages/FriendList.qml" line="25"></location>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Seaded</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendList.qml" line="29"></location>
         <source>Switch profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Vaheta profiil</translation>
     </message>
     <message>
         <location filename="../qml/pages/FriendList.qml" line="40"></location>
         <source>Add a friend</source>
-        <translation type="unfinished"></translation>
+        <translation>Lisa sõber</translation>
     </message>
 </context>
 <context>
@@ -319,12 +319,12 @@
     <message>
         <location filename="../qml/pages/LoadChatHistory.qml" line="39"></location>
         <source>From</source>
-        <translation type="unfinished"></translation>
+        <translation>Alates</translation>
     </message>
     <message>
         <location filename="../qml/pages/LoadChatHistory.qml" line="56"></location>
         <source>To</source>
-        <translation type="unfinished"></translation>
+        <translation>Kuni</translation>
     </message>
 </context>
 <context>
@@ -332,50 +332,50 @@
     <message>
         <location filename="../qml/pages/Profile.qml" line="35"></location>
         <source>Delete profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Kustuta profiil</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="37"></location>
         <source>Deleting profile</source>
-        <translation type="unfinished"></translation>
+        <translation>Profiili kustutamine</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="60"></location>
         <source>Profile Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Profiili nimi</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="75"></location>
         <source>Name</source>
         <extracomment>username for this profile</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Nimi</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="87"></location>
         <source>Status Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Olekuteade</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="104"></location>
         <source>Online</source>
         <extracomment>user status</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Onlainis</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="105"></location>
         <source>Away</source>
-        <translation type="unfinished"></translation>
+        <translation>Eemal</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="106"></location>
         <source>Busy</source>
-        <translation type="unfinished"></translation>
+        <translation>Hõivatud</translation>
     </message>
     <message>
         <location filename="../qml/pages/Profile.qml" line="152"></location>
         <location filename="../qml/pages/Profile.qml" line="155"></location>
         <source>Copy my Tox ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopeeri mu Tox ID</translation>
     </message>
 </context>
 <context>
@@ -383,42 +383,42 @@
     <message>
         <location filename="../qml/pages/Settings.qml" line="30"></location>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Seaded</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="18"></location>
         <source>Wifi only</source>
-        <translation type="unfinished"></translation>
+        <translation>Ainult WiFi</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="20"></location>
         <source>Send typing notifications?</source>
-        <translation type="unfinished"></translation>
+        <translation>Kas saata kirjutamise teatiseid?</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="22"></location>
         <source>Keep chat history</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoia vestluse ajalugu</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="24"></location>
         <source>Enable UDP</source>
-        <translation type="unfinished"></translation>
+        <translation>Luba UDP kasutamine</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="26"></location>
         <source>Notify me when...</source>
-        <translation type="unfinished"></translation>
+        <translation>Teavita mind, kui...</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="27"></location>
         <source>I receive a message</source>
-        <translation type="unfinished"></translation>
+        <translation>Ma saan  sõnumi</translation>
     </message>
     <message>
         <location filename="../src/settings.cpp" line="29"></location>
         <source>I receive a friend request</source>
-        <translation type="unfinished"></translation>
+        <translation>Ma saan sõbrataotluse</translation>
     </message>
 </context>
 <context>
@@ -426,17 +426,17 @@
     <message>
         <location filename="../qml/cyanide.qml" line="216"></location>
         <source>Incoming file: </source>
-        <translation type="unfinished"></translation>
+        <translation>Sissetulev fail: </translation>
     </message>
     <message>
         <location filename="../qml/cyanide.qml" line="229"></location>
         <source>is calling</source>
-        <translation type="unfinished"></translation>
+        <translation>helistab</translation>
     </message>
     <message>
         <location filename="../qml/cyanide.qml" line="244"></location>
         <source>Friend request received!</source>
-        <translation type="unfinished"></translation>
+        <translation>Sõbrataotlus saadud!</translation>
     </message>
 </context>
 </TS>
